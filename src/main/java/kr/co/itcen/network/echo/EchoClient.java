01c53c206e87dbddee5e71c6816de1f6c61b6a2b
JeaGunix <dlwprn1541@gmail.com>
2019-09-02 09:29:38 +0900

    http Server 추가

--- src/main/java/kr/co/itcen/network/echo/EchoClient.java
+++ src/main/java/kr/co/itcen/network/echo/EchoClient.java
@@ -12,7 +12,7 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class EchoClient {
-	private static String SERVER_IP = "192.168.1.19";
+	private static String SERVER_IP = "192.168.1.64";
 	public static int SERVER_PORT  = 8000;
 
 	public static void main(String[] args) {
